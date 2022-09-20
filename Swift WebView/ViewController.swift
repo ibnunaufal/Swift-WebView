15f9118d947e1f4282774c136b5735fe5915c66e
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-20 14:24:51 +0700

    implement webview

--- Swift WebView/ViewController.swift
+++ Swift WebView/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var inputtedUrl = ""
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.insetsLayoutMarginsFromSafeArea = false
         // Do any additional setup after loading the view.
+        
+        print("viewDidLoad function")
+        // create refresh button
+        self.inputtedUrl = "https://teknologikartu.com"
+        loadWebView()
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(self.webView.reload))
+        
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+        
     }
-
+    
+    
+    func loadWebView(){
+        print("loadWebView function")
+        if(!inputtedUrl.isEmpty){
+            let url = URL(string: inputtedUrl)!
+            webView.load(URLRequest(url: url))
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
