586cb0d317be5743fdf778daf055bc3a4b2933d2
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-20 10:54:20 +0700

    Initial Commit

---
+++ Swift WebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift WebView
+//
+//  Created by mac on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
